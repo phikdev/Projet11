acf34d90aa6804c4b77255628dba08aadc0a9183
phik <phik.dev@gmail.com>
2024-04-28 19:39:35 +0200

    etape1

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+
+
+
+defined( 'ABSPATH' ) || exit;
